4ac23c3964a10d557cbff2bb9c8313515e201406
ibuki2003 <ibuki426.sp@gmail.com>
2018-10-31 21:42:05 +0900

    マイグレーションファイルを編集

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('id');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
