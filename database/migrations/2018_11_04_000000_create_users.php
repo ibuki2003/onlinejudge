f673440206e0ff2881311cd947fa3e6453bec458
ibuki2003 <ibuki426.sp@gmail.com>
2018-11-04 18:21:23 +0900

    ユーザーテーブルのマイグレーションに旧OJとの互換性をもたせた

---
+++ database/migrations/2018_11_04_000000_create_users.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->string('id',16);
+                $table->primary('id');
+                $table->string('password');
+                $table->rememberToken();
+                $table->timestamps();
+            });
+        }else{
+            Schema::table('users', function (Blueprint $table) {
+                $table->rememberToken();
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
