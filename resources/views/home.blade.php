be33d464d5956a90bcaf9b98a0a3ba16ff7fb38b
ibuki2003 <ibuki426.sp@gmail.com>
2018-11-07 16:22:00 +0900

    トップにメニューをつけた

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.page')
 @section('title', config('app.name', 'Laravel'))
 @section('content')
-You are logged in!
+<ul class="nav flex-column">
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('problemList')}}">{{__('name.problemList')}}</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('allSubmissions')}}">{{__('name.submissions.all')}}</a>
+    </li>
+</ul>
+
 @endsection
