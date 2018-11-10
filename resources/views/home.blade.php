a63bfe423aa44523fe99431e0f5c673fe1436b4e
ibuki2003 <ibuki426.sp@gmail.com>
2018-11-10 16:43:11 +0900

    メニューを権限ごとに内容を変えるように変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,12 +2,19 @@
 @section('title', config('app.name', 'Laravel'))
 @section('content')
 <ul class="nav flex-column">
-    <li class="nav-item">
-        <a class="nav-link" href="{{route('problems')}}">{{__('name.problemList')}}</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="{{route('submissions')}}">{{__('name.submissions.all')}}</a>
-    </li>
+    @if (auth()->user()->has_permission('submit'))
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('problems')}}">{{__('name.problemList')}}</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('submissions_me')}}">{{__('name.submissions.me')}}</a>
+        </li>
+    @endif
+    @if (auth()->user()->has_permission('admit_users'))
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('submissions')}}">{{__('name.submissions.all')}}</a>
+        </li>
+    @endif
 </ul>
 
 @endsection
