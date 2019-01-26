62a0874bec47ef28c0eb8eee6f7ef2d19b1af0d4
ibuki2003 <ibuki426.sp@gmail.com>
2019-01-26 06:55:11 +0000

    集計APIのパラメータを自由に指定できるようにした

--- routes/statefulApi.php
+++ routes/statefulApi.php
@@ -5,11 +5,4 @@ use Illuminate\Http\Request;
 Route::get('/submissions', 'SubmissionController@allSubmissionsApi')->middleware('permission:admit_users');
 Route::get('/submissions/me', 'SubmissionController@mySubmissionsApi')->middleware('web');
 
-Route::prefix('statistics')->group(function () {
-    Route::get('problem_creator', 'StatisticsController@problemCreatorApi');
-    Route::get('problem_difficulty', 'StatisticsController@problemDifficultyApi');
-
-    Route::get('submission_status', 'StatisticsController@submissionStatusApi');
-    Route::get('submission_lang', 'StatisticsController@submissionLangApi');
-    Route::get('submission_user', 'StatisticsController@submissionUserApi');
-});
+Route::get('aggregate', 'StatisticsController@aggregateApi');
