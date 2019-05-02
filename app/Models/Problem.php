fde21f88153d6475fbe44ee24689da7f583c4dfc
ibuki2003 <ibuki426.sp@gmail.com>
2019-05-02 20:27:30 +0900

    fix typo at 7f8ca84

--- app/Models/Problem.php
+++ app/Models/Problem.php
@@ -66,8 +66,8 @@ class Problem extends Model
             if($zip->locateName('out/')!==FALSE)
                 Storage::disk('data')->deleteDirectory($base_dir . 'out');
 
-            if(Storage::disk('data')->exists($vase_dir . 'judge'))
-                Storage::disk('data')->delete($vase_dir . 'judge');
+            if(Storage::disk('data')->exists($base_dir . 'judge'))
+                Storage::disk('data')->delete($base_dir . 'judge');
 
             Storage::disk('data')->zipExtractTo($zip, $base_dir);
             unlink($filepath);
