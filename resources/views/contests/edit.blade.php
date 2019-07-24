63c838d506f7e667f4207fdf20690d340dbd1d59
ibuki2003 <ibuki426.sp@gmail.com>
2019-07-24 17:45:37 +0900

    escape vue mustaches

--- resources/views/contests/edit.blade.php
+++ resources/views/contests/edit.blade.php
@@ -6,12 +6,12 @@
 <div id="problems-controller">
     <div class="form-group">
         <label for="title-input">{{__('ui.contest.title')}}</label>
-        <input type="text" id="title-input" name="title" class="form-control" value="{{$contest->title}}" required>
+        <input type="text" id="title-input" name="title" class="form-control" value="@{{ {!!str_replace('}', '\}', json_encode($contest->title))!!} }}" required>
         <div class="invalid-feedback" id="title-error"></div>
     </div>
     <div class="form-group">
         <label for="description-input">{{__('ui.contest.description_optional')}}</label>
-        <textarea id="description-input" name="description" class="form-control">{{$contest->description}}</textarea>
+        <textarea id="description-input" name="description" class="form-control">@{{ {!!str_replace('}', '\}', json_encode($contest->description))!!} }}</textarea>
         <div class="invalid-feedback" id="description-error"></div>
     </div>
     <div class="form-group">
@@ -34,7 +34,7 @@
     <div class="input-group m-2">
         <select class="form-control" id="problem_new" v-model="problem_new">
             @foreach($problems as $problem)
-                <option value="{{$problem->id}}">{{$problem->id.' : '.$problem->title}}</option>
+                <option value="{{$problem->id}}">{{$problem->id}} : @{{ {!!str_replace('}', '\}', json_encode($problem->title))!!} }}</option>
             @endforeach
         </select>
         <div class="input-group-append">
@@ -92,7 +92,7 @@
                 @foreach(explode(',', $contest->problem_ids) as $index => $problem_id)
                     {id : {{$problem_id}}, point : {{explode(',', $contest->problem_points)[$index]}}},
                 @endforeach
-            ], 
+            ],
             problem_titles : {
                 @foreach ($problems as $problem)
                 {{$problem->id}}:'{{$problem->title}}',
@@ -166,12 +166,11 @@
                     }
                 }.bind(this));
             },
-            
+
         },
         updated : function() {
-            
+
         }
     });
 </script>
 @endsection
-
